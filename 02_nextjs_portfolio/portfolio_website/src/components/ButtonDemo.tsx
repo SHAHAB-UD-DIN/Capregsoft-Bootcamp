5663f884cc91bd6512b5a8230e7e5abc4e149cf4
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 17:42:04 +0500

    change button name

--- 02_nextjs_portfolio/portfolio_website/src/components/ButtonDemo.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/components/ButtonDemo.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
 
 export function ButtonDemo() {
-  return <Button variant="outline">Outline</Button>
+  return <Button variant="outline">Contact</Button>
 }
