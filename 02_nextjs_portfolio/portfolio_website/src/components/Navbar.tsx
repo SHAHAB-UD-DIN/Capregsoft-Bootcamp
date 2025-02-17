08a2c079f3eeb6e4d5cad69a85c065e7d7f6273b
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 17:07:06 +0500

    add darkmode

--- 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
@@ -1,12 +1,23 @@
 import Menu from "./Menu"
 import { ButtonDemo } from "./ButtonDemo"
+import  DarkMode  from "./DarkMode"
+
+
 
 export default function Navbar() {
     return (
-        <header className="grid grid-cols-[auto,auto] md:grid-cols-[auto,auto,auto] justify-items-center">
-         icon   
-        <Menu />
-        <ButtonDemo />
+        <header className="grid grid-cols-[auto,auto] md:grid-cols-[auto,auto,auto] justify-between items-center w-screen ">
+            icons
+
+        <div className="hidden md:flex justify-center gap-x-3">
+            <Menu />
+        </div>
+        
+        <div className="justify-self-start gap-x-3">
+            <DarkMode />
+            <ButtonDemo />
+        </div>
+
         </header>
     )
 }
