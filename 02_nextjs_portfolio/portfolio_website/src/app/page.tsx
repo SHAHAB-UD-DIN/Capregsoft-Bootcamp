f9c2a3eb5a7980141df1502686fb9d54cb076e28
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 11:53:38 +0500

    Update page.tsx

--- 02_nextjs_portfolio/portfolio_website/src/app/page.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <>
-    <main>
-      
-    </main>
+    <div className="items-center m-10">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Hello I'm 
+          <span className="text-blue-600 font-bold "> SHAHAB UD DIN</span>
+        </h1>
+        <h3 className="text-3xl font-semibold ">And I'm a Web Developer</h3>
+      </div>
+
+      <div className="justify-items-center">
+        <Image src="/pic.jpeg" alt="Profile" width={300} height={300} className="rounded-3xl p m-10 " />
+      </div>
+    </div>
+    <div>
+
+    </div>
     </>
   );
 }
