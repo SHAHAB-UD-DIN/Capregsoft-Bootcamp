dcd39a3ce3d9c2b235dcdceecda79d6e0eb45d82
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 15:50:29 +0500

    add Button

--- 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Menu from "./Menu"
+import { ButtonDemo } from "./ButtonDemo"
 
 export default function Navbar() {
     return (
-        <header className="grid grid-cols-[auto,auto]">
+        <header className="grid grid-cols-[auto,auto] md:grid-cols-[auto,auto,auto] justify-items-center">
          icon   
         <Menu />
+        <ButtonDemo />
         </header>
     )
 }
