7c909c54c1ba3e962129e1add2a46b8cdaaa7eb5
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 15:24:33 +0500

    Update Navbar.tsx

--- 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-
+import Menu from "./Menu"
 
 export default function Navbar() {
     return (
         <header className="grid grid-cols-[auto,auto]">
-        
+         icon   
+        <Menu />
         </header>
     )
 }
