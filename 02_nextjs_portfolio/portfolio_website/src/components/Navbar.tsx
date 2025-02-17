11f50a15d95fc7a310de8982c40928ff63ae6f3f
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-17 17:44:50 +0500

    add hamburger in navbar

--- 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Menu from "./Menu"
 import { ButtonDemo } from "./ButtonDemo"
 import  DarkMode  from "./DarkMode"
-
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 
 
 export default function Navbar() {
     return (
         <header className="grid grid-cols-[auto,auto] md:grid-cols-[auto,auto,auto] justify-between items-center w-screen ">
-            icons
+        <div className="flex justify-center">
+            <HamburgerMenuIcon className="md:hidden" />
+            icon
+        </div>
 
         <div className="hidden md:flex justify-center gap-x-3">
             <Menu />
