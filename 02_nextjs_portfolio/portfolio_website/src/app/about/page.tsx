3fd169de21cb8ce3a40bc67f343da3deff0fe414
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-19 05:47:00 +0500

    Changes

--- 02_nextjs_portfolio/portfolio_website/src/app/about/page.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/app/about/page.tsx
@@ -2,19 +2,18 @@ import Image from "next/image"
 
 export default function About(){
     return(
-        <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-            <div className="justify-items-center text-centre mb-2 text-lg mt-7">
+        <>
+        <div className="justify-center text-center m-10">
                 <h1 className="text-base">Introduction</h1>
                 <h2 className="text-3xl font-semibold">About me</h2>
-            </div>
+        </div>
 
-            <div className="flex w-full flex-cols lg:flex-row items-center gap-20 my-20">
-                <div>
+        <div className="flex bg-slate-100 max-w-7xl mx-auto m-4 rounded-lg p-4">
+            <div className="p-3">
                     <Image src="/pic.jpeg" alt="Profile" width={300} height={300} className="rounded-3xl" />
-                </div>
             </div>
 
-            <div className="flex items-center even:flex-row-reverse even:text-right">
+            <div className="p-3">
                 <p >
                 I am Shahab Ud Din, Frontend Developer Starting my career at Capresoft, I’ve focused on developing 
                 responsive web applications. My expertise lies in merging modern frontend technologies
@@ -23,6 +22,7 @@ export default function About(){
             </div>
 
         </div>
+        </>
     )
 
 }
