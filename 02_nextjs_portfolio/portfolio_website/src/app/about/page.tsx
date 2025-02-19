01191e275e49c0a893642b3cd24045902b8879b5
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-19 07:21:17 +0500

    Update page.tsx

--- 02_nextjs_portfolio/portfolio_website/src/app/about/page.tsx
+++ 02_nextjs_portfolio/portfolio_website/src/app/about/page.tsx
@@ -8,17 +8,21 @@ export default function About(){
                 <h2 className="text-3xl font-semibold">About me</h2>
         </div>
 
-        <div className="flex bg-slate-100 max-w-7xl mx-auto m-4 rounded-lg p-4">
-            <div className="p-3">
-                    <Image src="/pic.jpeg" alt="Profile" width={300} height={300} className="rounded-3xl" />
+        <div className="flex justify-center items-center rounded-xl">
+            <div>
+                    <Image src="/pic.jpeg" alt="Profile" width={250} height={250} className="rounded-3xl" />
             </div>
+        </div>
 
-            <div className="p-3">
-                <p >
-                I am Shahab Ud Din, Frontend Developer Starting my career at Capresoft, I’ve focused on developing 
+        <div className="bg-slate-100 max-w-2xl mx-auto mt-5 p-3 rounded-xl">
+            <div className="">
+                <h1 className="font-semibold">I am Shahab Ud Din, Frontend Developer</h1>
+                <p className="text-wrap">
+                Starting my career at Capresoft, I’ve focused on developing 
                 responsive web applications. My expertise lies in merging modern frontend technologies
                  with intelligent automation to enhance user experience and streamline processes.
                 </p>
+                <h2 className="">Skills</h2>
             </div>
 
         </div>
